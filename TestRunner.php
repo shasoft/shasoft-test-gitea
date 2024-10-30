b2edd26e73c5d68938849a36618a9af51f5f1a59
Shabanov Valera <git-shasoft-test-gitea@shasoft.com>
2024-10-30 21:53:21 +0300

    !!!

--- TestRunner.php
+++ TestRunner.php
@@ -2,7 +2,7 @@
 
 abstract class TestRunner
 {
-    // Конструктор aaa!
+    // Конструктор aaa!!!
     public function __construct(protected string $path) {}
     // Запустить тесты
     abstract public function run(\RunManager $runManager): string;
